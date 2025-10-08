0fd3e44e291cada702fb53bf03a98c58c220f9c9
Bradley Dice <bdice@bradleydice.com>
2025-10-08 13:24:46 -0500

    Run tests, then rebuild without tests before installing

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,17 +8,28 @@ REM Create build directory
 if not exist build mkdir build
 cd build
 
-REM Configure with cmake
+REM Configure with cmake (tests enabled)
 cmake -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DBUILD_SHARED_LIBS=ON ^
     -DROARING_DISABLE_NATIVE=ON ^
+    -DENABLE_ROARING_TESTS=ON ^
     %SRC_DIR%
 
-REM Build and install
-cmake --build . --target install
-
-REM Run tests
+REM Build and run tests
 cmake --build . --target test
+
+REM Clean artifacts from first build, to avoid installing tests
+cmake --build . --target clean
+
+REM Reconfigure with tests disabled, then install
+cmake -G "Ninja" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DROARING_DISABLE_NATIVE=ON ^
+    -DENABLE_ROARING_TESTS=OFF ^
+    %SRC_DIR%
+
+cmake --build . --target install
 
 endlocal
--- recipe/build.sh
+++ recipe/build.sh
@@ -15,20 +15,31 @@ fi
 mkdir -p build
 cd build
 
-# Configure with cmake
+# Run tests
+if [[ "${build_platform}" == "osx-64" && "${target_platform}" == "osx-arm64" ]]; then
+    echo "Skipping tests, osx-arm64 tests are not runnable on osx-64"
+else
+    # Configure with cmake (tests enabled)
+    cmake -G "Ninja" \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+        -DBUILD_SHARED_LIBS=ON \
+        -DROARING_DISABLE_NATIVE=ON \
+        -DENABLE_ROARING_TESTS=ON \
+        ${SRC_DIR}
+    cmake --build . --target test
+fi
+
+# Clean artifacts from first build, to avoid installing tests
+cmake --build . --target clean
+
+# Reconfigure with tests disabled, then install
 cmake -G "Ninja" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
     -DBUILD_SHARED_LIBS=ON \
     -DROARING_DISABLE_NATIVE=ON \
+    -DENABLE_ROARING_TESTS=OFF \
     ${SRC_DIR}
 
-# Build and install
 cmake --build . --target install
-
-# Run tests
-if [[ "${build_platform}" == "osx-64" && "${target_platform}" == "osx-arm64" ]]; then
-    echo "Skipping tests, osx-arm64 tests are not runnable on osx-64"
-else
-    cmake --build . --target test
-fi
